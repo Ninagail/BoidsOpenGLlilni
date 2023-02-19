b1b8db4ac8b0be634c5c18223003b001c5cb67cf
Jules Fouchy <jules.fouchy@ntymail.com>
2023-02-19 15:07:13 +0100

    ✨ TP1 EX2

--- src-common/default_shader.cpp
+++ src-common/default_shader.cpp
@@ -1,12 +1,30 @@
 #include "default_shader.hpp"
 #include "glad/gl.h"
 
-static unsigned int create_default_shader(const char* const vertexShaderSource, const char* const framentShaderSource)
+static unsigned int create_default_shader()
 {
+    const char* const vertexShaderSource = R"STR(
+        #version 330 core
+        layout (location = 0) in vec2 aPos;
+        
+        void main()
+        {
+           gl_Position = vec4(aPos, 0., 1.);
+        }
+    )STR";
+
     const unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
     glCompileShader(vertexShader);
-    const unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const unsigned int fragmentShader      = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* const  framentShaderSource = R"STR(
+        #version 330 core
+        out vec4 FragColor;
+        void main()
+        {
+            FragColor = vec4(1.f);
+        }
+    )STR";
     glShaderSource(fragmentShader, 1, &framentShaderSource, nullptr);
     glCompileShader(fragmentShader);
 
@@ -21,55 +39,8 @@ static unsigned int create_default_shader(const char* const vertexShaderSource, 
     return shaderProgram;
 }
 
-void bind_default_white_shader()
+void bind_default_shader()
 {
-    static unsigned int default_shader = create_default_shader(R"STR(
-        #version 330 core
-        layout (location = 0) in vec2 aPos;
-        
-        void main()
-        {
-           gl_Position = vec4(aPos, 0., 1.);
-        }
-    )STR",
-                                                               R"STR(
-        #version 330 core
-        out vec4 FragColor;
-        void main()
-        {
-            FragColor = vec4(1.f);
-        }
-    )STR");
-    glUseProgram(default_shader);
-}
-
-void bind_default_colored_shader()
-{
-    static unsigned int default_shader = create_default_shader(R"STR(
-        #version 330
-
-        layout(location = 3) in vec2 aVertexPosition;
-        layout(location = 8) in vec3 aVertexColor;
-
-        out vec3 vColor;
-
-        void main()
-        {
-            vColor      = aVertexColor;
-            gl_Position = vec4(aVertexPosition, 0, 1);
-        }
-    )STR",
-                                                               R"STR(
-        #version 330
-
-        in vec3 vColor;
-
-        out vec3 fFragColor;
-
-        void main()
-        {
-            fFragColor = vColor;
-        }
-    )STR");
+    static unsigned int default_shader = create_default_shader();
     glUseProgram(default_shader);
 }
--- src-common/default_shader.hpp
+++ src-common/default_shader.hpp
@@ -2,8 +2,4 @@
 
 /// Cette fonction bind un shader tout simple.
 /// Elle vous servira pour l'exercice 1 du TP1 car nous n'aurons pas encore vu comment faire nos shaders nous-même.
-void bind_default_white_shader();
-
-/// Cette fonction bind un shader tout simple.
-/// Elle vous servira pour l'exercice 2 du TP1 car nous n'aurons pas encore vu comment faire nos shaders nous-même.
-void bind_default_colored_shader();
+void bind_default_shader();
