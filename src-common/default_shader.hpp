f0348649295a12be31879af89e9c403b1596b9c3
Jules Fouchy <jules.fouchy@ntymail.com>
2023-02-19 13:53:13 +0100

    ✨ Add bind_default_shader()

---
+++ src-common/default_shader.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+/// Cette fonction bind un shader tout simple.
+/// Elle vous servira pour l'exercice 1 du TP1 car nous n'aurons pas encore vu comment faire nos shaders nous-même.
+void bind_default_shader();
