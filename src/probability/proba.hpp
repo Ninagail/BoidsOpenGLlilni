1e6455cd277ed361986beb9221086f09041d0935
LilouBERNARD <122173917+LilouBERNARD@users.noreply.github.com>
2024-04-18 13:39:46 +0200

    proba 1

---
+++ src/probability/proba.hpp
@@ -0,0 +1,18 @@
+#ifndef PROBABILITY_H
+#define PROBABILITY_H
+
+#include "elementdecor.hpp"
+
+double RAND();
+double simulateColorVariation(double mu, double b, double time);
+double simulateDecorSize(double alpha, double x_min);
+double cauchy_distribution(double location, double scale);
+void   appliquerRafaleVent(ElementDecor& elementDecor, double location, double scale);
+int    geometric_distribution(double p);
+bool   nouvelElementDecorApparait(double p);
+int    poisson_distribution(double lambda);
+bool   elementDecorDisparait(double lambda);
+int    binomialDistribution(int n, double p);
+int    simulerTempete(double probaTempete, int seuil, int impact);
+
+#endif
